d878c8960d0d6967397a6735405e9dd94e8fe852
duseth <ilyas.salimov.07@gmail.com>
2023-01-14 18:51:11 +0300

    Add middleware for private requests & change api schema

--- server/main.go
+++ server/main.go
@@ -3,38 +3,69 @@ package main
 import (
 	"log"
 
+	_ "github.com/duseth/istinara/server/docs"
 	"github.com/duseth/istinara/server/middlewares"
 	"github.com/duseth/istinara/server/models"
 	"github.com/duseth/istinara/server/routes"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
 )
 
+//	@title			Istinara API
+//	@version		1.0
+//	@description	Open Dictionary of Culturally Marked Vocabulary of the Russian Language with Arabic Translation
+
+//	@contact.name	Ilyas Salimov
+//	@contact.url	https://t.me/duseth
+//	@contact.email	ilyas.salimov.07@gmail.com
+
+//	@license.name	MIT License
+//	@license.url	https://spdx.org/licenses/MIT.html
+
+// @host		localhost:8080
+// @BasePath	/api
 func main() {
-	// Initialise Database
+	// Initialise database
 	models.DatabaseConnect()
 
-	// Public API
+	// Initialise router
 	router := gin.Default()
+
+	// Public API
 	api := router.Group("/api")
 	{
+		// Initialise Swagger docs
+		api.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 		// Global Middlewares
 		api.Use(cors.Default())
 
-		routes.AuthorPublic(api)
-		routes.WorkPublic(api)
-		routes.ArticlesPublic(api)
-
+		// Authentication
 		auth := api.Group("/auth")
 		{
 			routes.Auth(auth)
 		}
 
-		// Protected API
-		private := api.Group("/api/private")
+		routes.AuthorPublic(api)
+		routes.WorkPublic(api)
+		routes.ArticlesPublic(api)
+
+		// User API
+		user := api.Group("/user")
 		{
 			// Protected Middlewares
-			private.Use(middlewares.JwtAuthMiddleware())
+			user.Use(middlewares.JwtAuthMiddleware())
+
+			// TODO: user defined routes (e.g. favourites articles)
+		}
+
+		// Administration API
+		private := api.Group("/private")
+		{
+			// Private Middlewares
+			private.Use(middlewares.DataManipulationMiddleware())
 
 			routes.AuthorPrivate(private)
 			routes.WorkPrivate(private)
--- server/middlewares/middlewares.go
+++ server/middlewares/middlewares.go
@@ -3,6 +3,9 @@ package middlewares
 import (
 	"net/http"
 
+	"github.com/duseth/istinara/server/models"
+	httputil "github.com/duseth/istinara/server/utils/http"
+
 	"github.com/duseth/istinara/server/utils/token"
 	"github.com/gin-gonic/gin"
 )
@@ -11,10 +14,32 @@ func JwtAuthMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		err := token.Validate(ctx)
 		if err != nil {
-			ctx.String(http.StatusUnauthorized, "Unauthorized")
-			ctx.Abort()
+			httputil.SendErrorResponseWithAbort(ctx, http.StatusUnauthorized, err)
 			return
 		}
+		ctx.Next()
+	}
+}
+
+func DataManipulationMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		uid, err := token.ExtractTokenID(ctx)
+		if err != nil {
+			httputil.SendErrorResponseWithAbort(ctx, http.StatusBadRequest, err)
+			return
+		}
+
+		isPrivileged, err := models.CheckIfUserIsPrivileged(uid)
+		if err != nil {
+			httputil.SendErrorResponseWithAbort(ctx, http.StatusBadRequest, err)
+			return
+		}
+
+		if !isPrivileged {
+			httputil.SendErrorResponseWithAbort(ctx, http.StatusForbidden, err)
+			return
+		}
+
 		ctx.Next()
 	}
 }
